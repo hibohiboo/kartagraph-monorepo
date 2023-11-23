860da4aa0168012e91e000fad5181db8bd0da0e6
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-23 19:18:49 +0900

    refactoring #3

--- app/src/pages/TutorialPage.tsx
+++ app/src/pages/TutorialPage.tsx
@@ -1,43 +1,17 @@
 import { getSrc } from '@kartagraph-app/domain/images/getSrc';
+import { useTutorialHooks } from '@kartagraph-app/hooks/useTutorialHooks';
 import Card from '@kartagraph-ui/components/Card/Card';
 import { GameFrame } from '@kartagraph-ui/index';
-import { useState } from 'react';
-function* messagGenerator() {
-  yield `「はい！ いらっしゃいませ！
-  初めて見る顔ですね！
-  冒険者の方ですか？」`;
-  yield `「そうですか！」`;
-}
-const messageGen = messagGenerator();
-const first = messageGen.next().value;
+
 export default function TutorialPage() {
-  const [gen, setGen] = useState(messageGen);
-  const [message, setMessage] = useState(first);
-  const exArgs = {
-    message:
-      (message && {
-        text: message,
-        image: getSrc('/images/characters/recept/laugh.png'),
-      }) ||
-      undefined,
-    background: {
-      src: getSrc('/images/backgrounds/adv_inn_2.png'),
-    },
-    onClickMessage: () => {
-      setMessage(gen.next().value);
-    },
-  };
+  const vm = useTutorialHooks();
   return (
-    <GameFrame {...exArgs}>
+    <GameFrame {...vm.frameArgs}>
       <Card
         name="宿の娘"
         src={getSrc('/images/characters/recept/laugh.png')}
         style={{ position: 'absolute', top: '50px', left: '100px' }}
-        onClick={() => {
-          const g = messagGenerator();
-          setMessage(g.next().value);
-          setGen(g);
-        }}
+        onClick={vm.cardClick}
       />
     </GameFrame>
   );
