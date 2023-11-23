26e3a3a9b831ae08ed7b49423df834ac5fc33c75
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-23 10:17:44 +0900

    add 中央寄席 #3

--- packages/ui/src/components/MessageWindow/index.tsx
+++ packages/ui/src/components/MessageWindow/index.tsx
@@ -1,19 +1,32 @@
 import styles from './index.module.css';
+type AlignItems = 'start' | 'center' | 'end';
+// | 'self-start'
+// | 'self-end'
+// | 'flex-start'
+// | 'flex-end'
+// | 'baseline'
+// | 'normal'
+// | 'stretch' // React.CSSProperties['alignItems'];
 interface MessageWindowProps {
   text: string;
   fontSize?: string;
   style?: React.CSSProperties;
   className?: string;
+  textVerticalAlign?: AlignItems;
 }
 export default function MessageWindow({
   text,
+  textVerticalAlign = 'center',
   fontSize = '14px',
   style,
   className,
 }: MessageWindowProps) {
   return (
     <div className={`${styles.window} ${className}`} style={style}>
-      <div className={styles.text} style={{ fontSize }}>
+      <div
+        className={styles.text}
+        style={{ fontSize, alignItems: textVerticalAlign }}
+      >
         {text}
       </div>
     </div>
