40fd32ea47803576207691eb1611ffd50f780234
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-28 23:27:16 +0900

    add 選択肢 #3

--- app/src/hooks/useTutorialHooks.ts
+++ app/src/hooks/useTutorialHooks.ts
@@ -5,12 +5,14 @@ function* messagGenerator() {
   初めて見る顔ですね！
   冒険者の方ですか？」`;
   yield `「そうですか！」`;
+  yield `「では！ 宿帳をかいてください」`;
 }
 const messageGen = messagGenerator();
 const first = messageGen.next().value;
 export const useTutorialHooks = () => {
   const [gen, setGen] = useState(messageGen);
   const [message, setMessage] = useState(first);
+  const [questions, setQuestions] = useState<string[]>([]);
   const frameArgs = {
     message:
       (message && {
@@ -22,7 +24,20 @@ export const useTutorialHooks = () => {
       src: getSrc('/images/backgrounds/adv_inn_2.png'),
     },
     onClickMessage: () => {
-      setMessage(gen.next().value);
+      const message = gen.next().value;
+      setMessage(message);
+      if (message === `「では！ 宿帳をかいてください」`) {
+        setQuestions(['いいよ', 'やめとく']);
+      }
+    },
+    selectItems: questions,
+    onSelected: (item: string) => {
+      if (item === 'いいよ') {
+        setMessage(`「では！ こちらになりまーす」`);
+      } else {
+        setMessage(`「では！ おととい来やがれ！」`);
+      }
+      setQuestions([]);
     },
   };
   const cardClick = useCallback(() => {
