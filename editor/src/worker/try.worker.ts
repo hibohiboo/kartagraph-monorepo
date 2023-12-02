a7c21e39371386c7b07a43f7f89f117d9504e8c0
hibohiboo <hibohiboo66+github@gmail.com>
2023-12-03 00:56:10 +0900

    web workerが状態を持てることを確認 #5

--- editor/src/worker/try.worker.ts
+++ editor/src/worker/try.worker.ts
@@ -1,9 +1,10 @@
+let i = 0;
 self.addEventListener('message', () => {
   // ここで重たい処理を実行
   // 例: 2秒間待機
   setTimeout(() => {
     // 処理が完了したら、結果をメインスレッドに送信
-    self.postMessage('Heavy process completed');
+    self.postMessage(`Heavy process completed: ${i++}`);
   }, 2000);
 });
 
