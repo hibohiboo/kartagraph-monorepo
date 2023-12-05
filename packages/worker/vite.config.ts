18baa9a30b7601eab76300735facb55a28c31fe6
hibohiboo <hibohiboo66+github@gmail.com>
2023-12-05 21:30:30 +0900

    fix web worker build #5

--- packages/worker/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    // viteのホットリロードのために、/で始める必要がある。
-    alias: [{ find: '@kartagraph-worker', replacement: '/src' }],
-  },
-});
