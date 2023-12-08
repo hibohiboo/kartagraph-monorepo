9677d87b8d39cec265cd9db1f9ae702943bc84a0
hibohiboo <hibohiboo66+github@gmail.com>
2023-12-09 01:41:39 +0900

    add test

--- editor/tests/store/scenario/update.test.ts
+++ editor/tests/store/scenario/update.test.ts
@@ -6,17 +6,6 @@ import { createStore } from 'jotai';
 import { defaultScene } from './template/defaultScene';
 
 describe('updateNextAtom', () => {
-  test('messageコマンドでmessageのみ更新できること', () => {
-    const store = createStore();
-    store.set(sceneDataAtom, defaultScene);
-    store.set(updateNextAtom, {
-      command: 'message',
-      payload: { text: `2`, image: '/B.png' },
-    });
-    const scene = store.get(sceneDataAtom);
-    expect(scene.message).toEqual({ text: `2`, image: '/B.png' });
-    expect(scene.background?.src).toBe('/BG2.png');
-  });
   test('waitコマンドでmessageがundefinedになること', () => {
     const store = createStore();
     store.set(sceneDataAtom, defaultScene);
@@ -38,5 +27,34 @@ describe('updateNextAtom', () => {
     const scene = store.get(sceneDataAtom);
     expect(scene.message).toEqual({ text: `1`, image: '/A.png' });
     expect(scene.select).toEqual([{ label: '1', next: '2' }]);
+  });
+  describe('message', () => {
+    test('messageコマンドでmessageのみ更新できること', () => {
+      const store = createStore();
+      store.set(sceneDataAtom, defaultScene);
+      store.set(updateNextAtom, {
+        command: 'message',
+        payload: { text: `2`, image: '/B.png' },
+      });
+      const scene = store.get(sceneDataAtom);
+      expect(scene.message).toEqual({ text: `2`, image: '/B.png' });
+      expect(scene.background?.src).toBe('/BG2.png');
+    });
+    test('messageコマンドにselectを含めることができること', () => {
+      const store = createStore();
+      store.set(sceneDataAtom, defaultScene);
+      store.set(updateNextAtom, {
+        command: 'message',
+        payload: {
+          text: `2`,
+          image: '/B.png',
+          select: [{ label: '1', next: '2' }],
+        },
+      });
+      const scene = store.get(sceneDataAtom);
+      expect(scene.message).toEqual({ text: `2`, image: '/B.png' });
+      expect(scene.background?.src).toBe('/BG2.png');
+      expect(scene.select).toEqual([{ label: '1', next: '2' }]);
+    });
   });
 });
