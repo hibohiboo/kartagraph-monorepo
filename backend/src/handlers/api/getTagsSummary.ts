6d41fdfc26af9fbd30173945a32b135db4e83e60
hibohiboo <hibohiboo66+github@gmail.com>
2023-12-11 23:27:08 +0900

    add deploy pages #9

--- backend/src/handlers/api/getTagsSummary.ts
+++ backend/src/handlers/api/getTagsSummary.ts
@@ -9,10 +9,12 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
     };
   }
   const ret = await execQuery(
-    `SELECT tag_name, count(*) count FROM tag_history where scenario_id = '${event.pathParameters.scenarioId}' group by scenario_id, tag_name`,
+    `SELECT tag_name, count(*) count FROM tag_history where scenario_id = '${event.pathParameters.scenarioId}' and tag_type <> 'secret' group by scenario_id, tag_name`,
   );
   return {
     statusCode: 200,
-    body: JSON.stringify(ret),
+    body: JSON.stringify(
+      ret.map((r) => ({ tagName: r.tag_name, userCount: Number(r.count) })),
+    ),
   };
 };
