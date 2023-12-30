7147b7e76db5982902c8f3ec97795e4f43b2153c
hibohiboo <hibohiboo66+github@gmail.com>
2023-12-30 19:13:08 +0900

    fix setting #18

--- packages/editor-ui/vite-project/vite.config.ts
+++ packages/editor-ui/vite-project/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: '@kartagraph-editor-ui', replacement: '/src' }],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react/jsx-runtime'],
+          others: ['sanitize-html'],
+        },
+      },
+    },
+  },
+});
