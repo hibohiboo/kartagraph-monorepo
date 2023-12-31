55b35ddd7a4e5d2d37c2ebdceeda0467aecc5f20
hibohiboo <hibohiboo66+github@gmail.com>
2023-12-31 14:51:01 +0900

    fix message 表示 #18

--- editor/src/domain/scenarioTree/components/ScenarioTree.tsx
+++ editor/src/domain/scenarioTree/components/ScenarioTree.tsx
@@ -1,11 +1,32 @@
 import { Tree } from 'primereact/tree';
+import { BiWindow } from 'react-icons/bi';
+import { BiWindows } from 'react-icons/bi';
 import { BsFilePlay } from 'react-icons/bs';
 import { FaImage } from 'react-icons/fa6';
 import { GiStabbedNote } from 'react-icons/gi';
 import { GiCardboardBox } from 'react-icons/gi';
 import { TbCards } from 'react-icons/tb';
+
 interface Card {
   name: string;
+}
+interface MessageEvent {
+  id: string;
+  type: 'message';
+  data: {
+    text: string;
+    image?: string;
+  };
+  next?: string;
+}
+interface MessagesEvent {
+  id: string;
+  type: 'message';
+  data: {
+    texts: string[];
+    image?: string;
+  };
+  next?: string;
 }
 interface Event {
   id: string;
@@ -22,6 +43,7 @@ interface Scenario {
   title: string;
   scenes: Scene[];
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function IconWithText({ icon, text }: { icon: any; text: string }) {
   return (
     <span style={{ display: 'inline-flex', alignItems: 'center' }}>
@@ -30,7 +52,57 @@ function IconWithText({ icon, text }: { icon: any; text: string }) {
     </span>
   );
 }
+function EllipsisText({ text }: { text: string }) {
+  return (
+    <span
+      style={{
+        width: '15rem',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        display: 'inline-block',
+      }}
+      title={text}
+    >
+      {text}
+    </span>
+  );
+}
+function MessageEventItem({ event }: { event: MessageEvent }) {
+  return (
+    <span style={{ display: 'inline-flex' }}>
+      <IconWithText icon={<BiWindow />} text={event.type} />
+      <EllipsisText text={event.data.text} />
+    </span>
+  );
+}
 const indentStyle = { marginLeft: '1rem' };
+function EventItem({ event }: { event: Event }) {
+  if (((e: Event): e is MessageEvent => event.type === 'message')(event)) {
+    return <MessageEventItem event={event} />;
+  }
+  if (((e: Event): e is MessagesEvent => event.type === 'messages')(event)) {
+    return (
+      <>
+        <IconWithText icon={<BiWindows />} text={event.type} />
+        <ul style={indentStyle}>
+          {event.data.texts.map((text, i) => (
+            <li key={`${text}-${i}`}>
+              <MessageEventItem
+                event={{
+                  id: `${event.id}-${i}`,
+                  type: 'message',
+                  data: { text },
+                }}
+              />
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+  return <IconWithText icon={<BsFilePlay />} text={event.type} />;
+}
 function Scene({ scene }: { scene: Scene }) {
   return (
     <>
@@ -40,7 +112,7 @@ function Scene({ scene }: { scene: Scene }) {
         <IconWithText icon={<GiCardboardBox />} text={'カード一覧'} />
         <ul style={indentStyle}>
           {scene.cards.map((card) => (
-            <li>
+            <li key={card.name}>
               <IconWithText icon={<TbCards />} text={card.name} />
             </li>
           ))}
@@ -50,8 +122,8 @@ function Scene({ scene }: { scene: Scene }) {
         <IconWithText icon={<BsFilePlay />} text={'イベント一覧'} />
         <ul style={indentStyle}>
           {scene.events.map((event) => (
-            <li>
-              <IconWithText icon={<TbCards />} text={event.type} />
+            <li key={event.id}>
+              <EventItem event={event} />
             </li>
           ))}
         </ul>
