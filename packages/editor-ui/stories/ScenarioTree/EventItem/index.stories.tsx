07b6a8803f18aa40f0d72e9a7930d2149c23cfea
hibohiboo <hibohiboo66+github@gmail.com>
2024-01-04 21:28:33 +0900

    fix イベント変換 #18

--- packages/editor-ui/stories/ScenarioTree/EventItem/index.stories.tsx
+++ packages/editor-ui/stories/ScenarioTree/EventItem/index.stories.tsx
@@ -1,5 +1,4 @@
 import { EventItem as Component } from '@kartagraph-editor-ui/components/ScenarioTree/molecules/EventItem';
-import { PCStory } from '../../constants';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const args = {
@@ -20,8 +19,61 @@ const meta = {
 } satisfies Meta<typeof Component>;
 export default meta;
 type Story = StoryObj<typeof meta>;
-export const Default: Story = {};
+export const Message: Story = {};
 
-export const PC: Story = {
-  ...PCStory,
+export const Messages: Story = {
+  args: {
+    event: {
+      id: 'event2',
+      type: 'messages',
+      data: {
+        texts: ['「あなたもお仕事ご苦労様です」', '「行ってらっしゃい！」'],
+        image: '/images/characters/recept/laugh.png',
+      },
+    },
+  },
+};
+export const Branch: Story = {
+  args: {
+    event: {
+      id: 'click1',
+      type: 'branch',
+      data: {
+        condition: 'hasTag',
+        tag: '風呂',
+        next: 'お風呂どうだった',
+      },
+      next: 'mesicheck',
+    },
+  },
+};
+export const AddTag: Story = {
+  args: {
+    event: {
+      id: 'click1',
+      type: 'addTag',
+      data: {
+        name: '風呂',
+      },
+    },
+  },
+};
+export const ChangeScene: Story = {
+  args: {
+    event: {
+      id: 'click1',
+      type: 'changeScene',
+      data: {
+        sceneId: 'scene2',
+      },
+    },
+  },
+};
+export const EndScenario: Story = {
+  args: {
+    event: {
+      id: 'click1',
+      type: 'endScenario',
+    },
+  },
 };
