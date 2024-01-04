5cafb6d4da8f2f2ccdee8c4a278a48254c35cbf5
hibohiboo <hibohiboo66+github@gmail.com>
2024-01-04 22:52:56 +0900

    add red test #18

--- editor/tests/domain/scenarioTree/converter.test.ts
+++ editor/tests/domain/scenarioTree/converter.test.ts
@@ -39,4 +39,34 @@ describe('convertScenario', () => {
     expect(ret.scenes[1].title).toBe('街');
     expect(ret.scenes[1].isFirstScene).toBe(false);
   });
+
+  test('初期イベントを変換できること', () => {
+    const ret = convertScenario({
+      ...scenario,
+      firstSceneId: 'scene1',
+      scenes: [
+        {
+          id: 'scene1',
+          title: '冒険者の宿',
+          eventId: 'event1',
+          events: [
+            {
+              id: 'event1',
+              type: 'message',
+              data: {
+                text: '「おはよう！\n今日もがんばろー」',
+                image: '/images/characters/recept/laugh.png',
+              },
+            },
+          ],
+          cards: [],
+        },
+      ],
+    });
+    const [scene1] = ret.scenes;
+    const [event1] = scene1.events;
+    expect(event1.id).toBe('event1');
+    expect(event1.type).toBe('message');
+    expect(event1.data.text).toBe('「おはよう！\n今日もがんばろー」');
+  });
 });
