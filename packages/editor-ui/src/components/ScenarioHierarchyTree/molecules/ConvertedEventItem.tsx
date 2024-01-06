561c42ed573b7e963772b4d10cbab21bc4eba659
hibohiboo <hibohiboo66+github@gmail.com>
2024-01-06 12:03:10 +0900

    fix 分岐の見た目を見やすく #18

--- packages/editor-ui/src/components/ScenarioHierarchyTree/molecules/ConvertedEventItem.tsx
+++ packages/editor-ui/src/components/ScenarioHierarchyTree/molecules/ConvertedEventItem.tsx
@@ -1,19 +1,45 @@
 import { indentStyle } from '@kartagraph-editor-ui/components/ScenarioTree/constants';
+import { FaRegCheckCircle } from 'react-icons/fa';
+import { FaRegTimesCircle } from 'react-icons/fa';
 import { ConvertedSceneEvent } from '../type';
 import { HierarchyTreeEventItem } from './EventItem';
-
 export default function ConvertedEventItem({
   event,
 }: {
   event: ConvertedSceneEvent;
 }) {
   if (!event) return <></>;
-
+  if (event.type === 'branch')
+    return <BranchConvertedEventItem event={event} />;
   return (
     <div style={indentStyle}>
       <HierarchyTreeEventItem event={event} />
       {event.next && typeof event.next !== 'string' && (
         <div style={indentStyle}>
+          <ConvertedEventItem event={event.next} />
+        </div>
+      )}
+    </div>
+  );
+}
+function BranchConvertedEventItem({ event }: { event: ConvertedSceneEvent }) {
+  const branchItemStyle = {
+    ...indentStyle,
+    display: 'flex',
+    alignItems: 'baseline',
+  };
+  return (
+    <div style={indentStyle}>
+      <HierarchyTreeEventItem event={event} />
+      {event.data && event.data.next && typeof event.data.next !== 'string' && (
+        <div style={branchItemStyle}>
+          <FaRegCheckCircle />
+          <ConvertedEventItem event={event.data.next} />
+        </div>
+      )}
+      {event.next && typeof event.next !== 'string' && (
+        <div style={branchItemStyle}>
+          <FaRegTimesCircle />
           <ConvertedEventItem event={event.next} />
         </div>
       )}
