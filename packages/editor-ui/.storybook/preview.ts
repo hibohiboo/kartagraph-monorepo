819814beb315a3caf604bb36ee043a5a4408b3d9
hibohiboo <hibohiboo66+github@gmail.com>
2024-01-21 16:49:08 +0900

    fix css

--- packages/editor-ui/.storybook/preview.ts
+++ packages/editor-ui/.storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
