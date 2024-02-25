4a847d8eb60c409692e91cee0ed4473e7ba636c2
hibo <hibohiboo66@gmail.com>
2024-02-25 15:22:42 +0900

    add test red #28

---
+++ backend/tests/domain/scenario/data/scenarioList.ts
@@ -0,0 +1,10 @@
+export const scenarioListItem = {
+  id: 'a9ff348b-8a64-4dae-b658-c05ac44413fa',
+  title: 'シナリオテスト',
+  src: '/images/backgrounds/adv_inn_2.png',
+  summary: 'テストサマリ',
+  detail: '詳細',
+  s3_key: 'scenarios/a9ff348b-8a64-4dae-b658-c05ac44413fa.json',
+  created: '2024-02-22T13:04:04.905Z',
+  updated: '2024-02-22T14:30:55.898Z',
+};
