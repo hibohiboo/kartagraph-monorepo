2e39f55d23e2b00645b0e5cfe1dcbdc18792c366
hibo <hibohiboo66@gmail.com>
2024-02-25 15:22:50 +0900

    add test green #28

--- backend/tests/domain/scenario/scenarioRepository.test.ts
+++ backend/tests/domain/scenario/scenarioRepository.test.ts
@@ -17,8 +17,8 @@ describe('getScenarioList', () => {
       summary: 'テストサマリ',
       detail: '詳細',
       s3_key: 'scenarios/a9ff348b-8a64-4dae-b658-c05ac44413fa.json',
-      created: '2024-02-22T13:04:04.905Z',
-      updated: '2024-02-22T14:30:55.898Z',
+      created: new Date('2024-02-22T13:04:04.905Z'),
+      updated: new Date('2024-02-22T14:30:55.898Z'),
     });
   });
 });
