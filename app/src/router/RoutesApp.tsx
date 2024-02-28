554e5191159998def87dd3061fd346eba84c5d25
hibo <hibohiboo66@gmail.com>
2024-02-28 21:53:44 +0900

    fix router #32

--- app/src/router/RoutesApp.tsx
+++ app/src/router/RoutesApp.tsx
@@ -3,9 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import { createRouter } from '.';
 
 export function RoutesApp() {
-  const memorizedRouter = useMemo(() => {
-    // mswの設定がされた後にrouterを作成する
-    return createRouter();
-  }, []);
-  return <RouterProvider router={memorizedRouter} future={{ v7_startTransition: true }} />;
+  // mswの設定がされた後にrouterを作成するためにhooks内でrouterを作成する
+  const router = useMemo(() => createRouter(), []);
+  return <RouterProvider router={router} future={{ v7_startTransition: true }} />;
 }
