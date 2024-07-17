750ba84734121f975bc292dc5bf9165571c90e92
hibohiboo <hibohiboo66+github@gmail.com>
2024-07-17 21:53:07 +0900

    fix editor debug

--- editor/src/domain/images/getSrc.ts
+++ editor/src/domain/images/getSrc.ts
@@ -4,5 +4,7 @@ export const getSrc = (src: string) => {
   // if (import.meta.env.DEV) {
   //   return src;
   // }
-  return `/${basePath}${src}`;
+  // srcの先頭に/がある場合は取り除く
+  const trimedSrc = src.startsWith('/') ? src.slice(1) : src;
+  return `/${basePath}/${trimedSrc}`;
 };
