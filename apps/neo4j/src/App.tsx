8570a22978b9e3530294770d9602d06989920963
hibohiboo <hibohiboo66+github@gmail.com>
2024-10-04 00:09:00 +0900

    add node #36

--- apps/neo4j/src/App.tsx
+++ apps/neo4j/src/App.tsx
@@ -1,15 +1,25 @@
 import { BasicNvlWrapper, InteractiveNvlWrapper } from '@neo4j-nvl/react';
+import { useState } from 'react';
+
+// https://neo4j.com/docs/nvl/current/react-wrappers/
 
 export function App() {
+  const [nodes, setNodes] = useState([{ id: '0' }, { id: '1' }]);
+
+  const addElements = () => {
+    const newNodes = [...nodes, { id: `${nodes.length}` }];
+    setNodes(newNodes);
+  };
   return (
     <div style={{ width: '100%', height: 500 }}>
       <h1>Hello, world!</h1>
       <BasicNvlWrapper
         nvlOptions={{ initialZoom: 2 }}
-        nodes={[{ id: '0' }, { id: '1' }]}
+        nodes={nodes}
         rels={[{ id: '10', from: '0', to: '1' }]}
         nvlCallbacks={{ onLayoutDone: () => console.log('layout done') }}
       />
+      <button onClick={addElements}>Add Graph Elements</button>
       <InteractiveNvlWrapper
         nvlOptions={{ useWebGL: false, initialZoom: 2.6 }}
         nodes={[
