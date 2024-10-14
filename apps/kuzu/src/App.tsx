128eefc5a0268bc2fe75ee88c581c9ce2912d2cc
hibohiboo <hibohiboo66+github@gmail.com>
2024-10-15 01:28:28 +0900

    add kuzu 追加 #36

--- apps/kuzu/src/App.tsx
+++ apps/kuzu/src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import kuzu_wasm from '@kuzu/kuzu-wasm';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    (async () => {
+      const kuzu = await kuzu_wasm();
+      const db = await kuzu.Database();
+      const conn = await kuzu.Connection(db);
+      await conn.execute(`CREATE NODE TABLE User(name STRING, age INT64, PRIMARY KEY (name))`);
+      await conn.execute(`CREATE (u:User {name: 'Alice', age: 35});`);
+      const res = await conn.execute(`MATCH (a:User) RETURN a.*;`);
+      const res_json = JSON.parse(res.table.toString());
+      console.log(res_json);
+    })();
+  }, []);
 
   return (
     <>
@@ -18,18 +31,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- apps/kuzu/vite.config.ts
+++ apps/kuzu/vite.config.ts
@@ -4,4 +4,16 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@kuzu/kuzu-wasm'],
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
 });
