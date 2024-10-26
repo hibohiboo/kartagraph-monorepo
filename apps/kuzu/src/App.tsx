099cb4c151c8aa83909f0c6c85374125fe6f932a
hibohiboo <hibohiboo66+github@gmail.com>
2024-10-27 02:09:09 +0900

    add export #36

--- apps/kuzu/src/App.tsx
+++ apps/kuzu/src/App.tsx
@@ -1,15 +1,57 @@
-import kuzu_wasm from '@kuzu/kuzu-wasm';
+import kuzu_wasm, { Connection, Kuzu } from '@kuzu/kuzu-wasm';
 import Cytoscape from 'cytoscape';
 import { useEffect, useRef, useState } from 'react';
 import { CytoscapeComponent } from './shared/ui';
+
 function App() {
   const [elements, setNElements] = useState<{ data: { id?: string; source?: string; target?: string; label?: string } }[]>([]);
   const cyref = useRef<Cytoscape.CoreLayout | null>(null);
+  const conRef = useRef<Connection | null>(null);
+  const kuzuRef = useRef<Kuzu | null>(null);
+  const draw = async () => {
+    if (!conRef.current) return;
+    // Execute Cypher query
+    const response = await conRef.current.execute(
+      `
+   MATCH (a:User)
+   RETURN *;
+   `,
+    );
+    const users = !response.table ? [] : JSON.parse(response.table.toString());
+    type ID = { offset: string; table: string };
+    const getId = (id: ID): string => `id_${id.table}_${id.offset}`;
+    const nodes = users.map(({ a }: { a: { _ID: ID; name: string } }) => ({ data: { id: getId(a._ID), label: a.name } }));
+
+    const relsResponse = await conRef.current.execute(
+      `
+   MATCH (a:User)-[r:Follows]->(b:User)
+   RETURN *;
+   `,
+    );
+    const rels = !relsResponse.table ? [] : JSON.parse(relsResponse.table.toString());
+
+    const relsData = rels.map(
+      ({ r }: { a: { _ID: ID; name: string }; r: { _ID: ID; _SRC: ID; _DST: ID; since: string }; b: { _ID: ID; name: string } }) => ({
+        data: {
+          source: getId(r._SRC),
+          target: getId(r._DST),
+          id: getId(r._ID),
+          label: r.since,
+        },
+      }),
+    );
+    setNElements([...nodes, ...relsData]);
+    setTimeout(() => {
+      if (cyref.current) cyref.current.layout({ name: 'grid' }).run();
+    }, 0);
+  };
   useEffect(() => {
     (async () => {
       const kuzu = await kuzu_wasm();
       const db = await kuzu.Database();
       const conn = await kuzu.Connection(db);
+      conRef.current = conn;
+      kuzuRef.current = kuzu;
 
       kuzu.FS.writeFile('/follows.csv', await (await fetch('/data/follows.csv')).text());
       kuzu.FS.writeFile('/city.csv', await (await fetch('/data/city.csv')).text());
@@ -29,51 +71,50 @@ function App() {
       await conn.execute('COPY LivesIn FROM "/lives-in.csv"');
 
       // Execute Cypher query
-      const response = await conn.execute(
-        `
-         MATCH (a:User)
-         RETURN *;
-         `,
-      );
-      const users = JSON.parse(response.table.toString());
-      type ID = { offset: string; table: string };
-      const getId = (id: ID): string => `id_${id.table}_${id.offset}`;
-      const nodes = users.map(({ a }: { a: { _ID: ID; name: string } }) => ({ data: { id: getId(a._ID), label: a.name } }));
-
-      const relsResponse = await conn.execute(
-        `
-         MATCH (a:User)-[r:Follows]->(b:User)
-         RETURN *;
-         `,
-      );
-      const rels = JSON.parse(relsResponse.table.toString());
-
-      const relsData = rels.map(
-        ({ r }: { a: { _ID: ID; name: string }; r: { _ID: ID; _SRC: ID; _DST: ID; since: string }; b: { _ID: ID; name: string } }) => ({
-          data: {
-            source: getId(r._SRC),
-            target: getId(r._DST),
-            id: getId(r._ID),
-            label: r.since,
-          },
-        }),
-      );
-      setNElements([...nodes, ...relsData]);
-      if (cyref.current) {
-        cyref.current.layout({ name: 'grid' }).run();
-      }
+      await draw();
     })();
   }, []);
   return (
-    <CytoscapeComponent
-      elements={elements}
-      cy={(cy) => {
-        cyref.current = cy;
-      }}
-      layout={{ name: 'grid' }}
-      wheelSensitivity={0.1}
-      style={{ width: '600px', height: '600px' }}
-    />
+    <>
+      <CytoscapeComponent
+        elements={elements}
+        cy={(cy) => {
+          cyref.current = cy;
+        }}
+        layout={{ name: 'grid' }}
+        wheelSensitivity={0.1}
+        style={{ width: '600px', height: '600px' }}
+      />
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log(e);
+          if (!conRef.current) return;
+          const form = new FormData(e.currentTarget);
+          const userName = form.get('username') || '';
+          if (!userName) return;
+          await conRef.current.execute(`CREATE (u:User {name: '${userName}', age: 10});`);
+          await draw();
+          return;
+        }}
+      >
+        <input type="text" name="username" />
+        <button type="submit">ユーザ追加</button>
+      </form>
+      <button
+        onClick={async () => {
+          if (!conRef.current || !kuzuRef.current) return;
+          // await conRef.current.execute(`EXPORT DATABASE '/bakcup.csv' (format="csv", header=true);`);
+          //
+          await conRef.current.execute(`COPY (MATCH (u:User) RETURN u.*) TO '/bakcup.csv' (header=true);`);
+
+          const contents = kuzuRef.current.FS.readFile('/bakcup.csv', { encoding: 'utf8' });
+          console.log('contents', contents);
+        }}
+      >
+        保存
+      </button>
+    </>
   );
 }
 
